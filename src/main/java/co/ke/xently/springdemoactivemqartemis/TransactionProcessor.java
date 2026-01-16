9228f3bba6882591024252d4d2f26afb3d007e0c
Orinda Harrison (Mitch) <mitchke@duck.com>
2026-01-16 11:57:30 +0300

    Optimize transaction processing: switch to `JmsClient`, increase transaction frequency, and raise max Artemis connections to 1000.

--- src/main/java/co/ke/xently/springdemoactivemqartemis/TransactionProcessor.java
+++ src/main/java/co/ke/xently/springdemoactivemqartemis/TransactionProcessor.java
@@ -3,28 +3,34 @@ package co.ke.xently.springdemoactivemqartemis;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.JmsClient;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 @Slf4j
 @AllArgsConstructor
 class TransactionProcessor {
-    private final JmsTemplate jmsTemplate;
+    private final JmsClient jmsClient;
 
-    @Scheduled(cron = "0 * * * * *")
+    @Scheduled(cron = "0/1 * * * * *")
     public void sendTransaction() {
-        log.info("Sending transaction");
-        var transaction = new Transaction(new Random().nextDouble());
-        jmsTemplate.convertAndSend("transactions", transaction);
-        log.info("Sent transaction {}", transaction);
+        int numberOfTransactions = new Random().nextInt(900, 1_000);
+        for (int i = 0; i < numberOfTransactions; i++) {
+            var transaction = new Transaction(new Random().nextDouble());
+            jmsClient.destination("transactions")
+                    .send(transaction);
+        }
+        log.info("Sent {} transactions; processed {}", numberOfTransactions, transactionCount.get());
     }
+
+    private final AtomicInteger transactionCount = new AtomicInteger(0);
 
     @JmsListener(destination = "transactions")
     public void processTransaction(final Transaction transaction) {
-        log.info("Processing transaction {}", transaction);
+        log.debug("[#{}] Processing transaction {}", transactionCount.incrementAndGet(), transaction);
     }
 }
